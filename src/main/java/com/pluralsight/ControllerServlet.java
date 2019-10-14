8b1ca5b8824a6128f7a92e84927e2d07dd59399f
peterlesko <peterlesko@yahoo.com>
2019-10-14 23:09:04 +0100

    Completed module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -65,9 +65,12 @@ public class ControllerServlet extends HttpServlet {
 		   		case "/edit":
 				   	 showEditForm(request, response);
 			break;
-        default:
+				case "/update":
+					 updateBook(request, response);
+			break;
+        	default:
 				   listBooks(request, response);
-           break;
+            break;
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -128,6 +131,18 @@ public class ControllerServlet extends HttpServlet {
 			dispatcher.forward(request, response);
 		}
 
+	private void updateBook(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException { 
+			int id = Integer.parseInt(request.getParameter("id"));
+			String title = request.getParameter("booktitle");
+			String author = request.getParameter("bookauthor");
+			String priceString = request.getParameter("bookprice");
+
+			Book newBook = new Book(id, title, author, Float.parseFloat(priceString));
+			bookDAO.updateBook(newBook);
+			response.sendRedirect("list");
+		}
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -137,5 +152,4 @@ public class ControllerServlet extends HttpServlet {
 		out.println("This is the doPost() method!");
 		doGet(request, response);
 	}
-
 }
